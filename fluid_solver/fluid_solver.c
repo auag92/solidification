00a7b33681ec246251799df56ead017e88cd4f26
auag92 <apaar92@gmail.com>
2015-03-12 00:48:41 +0530

    fluid solver stuff all together

--- fluid_solver/fluid_solver.c
+++ fluid_solver/fluid_solver.c
@@ -1,44 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define pmesh 16
-#define MESHX (pmesh + 3)
+#include "mg_solver.c"
+#include "math.h"
+#define pmesh 65
+#define pmesh2 (pmesh*pmesh)
+#define MESHX (pmesh + 1)
 #define MESHX2 (MESHX*MESHX)
 #define deltax (1)
 #define inv_deltax (1/deltax)
 #define inv_deltax2 (inv_deltax*inv_deltax)
 #define deltat (.01)
-#define ntimesteps (100000)
+#define ntimesteps (10000)
+#define saveT (100)
 
 #define Mu (1)
 #define re (100)
 #define inv_re (1/re)
 #define Uw (1) // wall velocity
 
-double P[MESHX2], Pstr[pmesh];
+double P[MESHX2];
 double u_old[MESHX2], u_now[MESHX2];
 double v_old[MESHX2], v_now[MESHX];
-double rhs_fn[pmesh];
+double rhs_fn[2*pmesh2];
 
 void initialize();
-void boundary();
+void boundary1();
 void update();
-void laplacian(double *f, double *lap);
+void laplacian(double *f, double *lap, int M);
 void RHS_fn(double *u, double *v, double *lap_u, double *lap_v, int M);
-void Pupdate();
+void write2file (long t, double *u, double *v, int M);
 
 main(){
 
-  int i,j,z,t=0;
+  int i,j,z,x,t=0;
   double du_dx,du_dy,dv_dx,dv_dy,dp_dx,dp_dy,du_dt,dv_dt;
   double lap_u[MESHX2], lap_v[MESHX2];
   initialize();
   for(t=0; t<ntimesteps; t++){
-    laplacian(*u_old,*lap_u, MESHX2);
-    laplacian(*v_old,*lap_v, MESHX2);
-    RHS_fn(*u_old,*v_old,*lap_u,*lap_v,MESHX2);
-    multigrid(*Pstr, *rhs_fn, pmesh);
-    Pupdate();
+    laplacian(u_old,lap_u, MESHX2);
+    laplacian(v_old,lap_v, MESHX2);
+    RHS_fn(u_old,v_old,lap_u,lap_v,MESHX2);
+    multigrid(P, rhs_fn);
     for(i=1; i<MESHX-1; i++){
       for(j=1; j<MESHX-1; j++){
 	
@@ -46,18 +48,22 @@ main(){
 	x = (i-1)*(MESHX-1)+ (j-1); 
 	du_dx = 0.5*inv_deltax*(u_old[z+1] - u_old[z-1]);
 	dv_dx = 0.5*inv_deltax*(v_old[z+MESHX] - v_old[z-MESHX]);
-	dp_dx = 0.5*inv_deltax*(P[x+1]+P[X+MESHX]-P[x+MESHX-1]-P[x]);
-	dp_dy = 0.5*inv_deltax*(P[x+MESHX]+P[X+MESHX-1]-P[x+1]-P[x]);
-        du_dt = mu*lap_u[z] - dp_dx - (u_old[z]*du_dx + v_old[z]*du_dy);
-        dv_dt = mu*lap_v[z] - dp_dy - (u_old[z]*dv_dx + v_old[z]*dv_dy);
+	dp_dx = 0.5*inv_deltax*(P[x+1]+P[x+MESHX]-P[x+MESHX-1]-P[x]);
+	dp_dy = 0.5*inv_deltax*(P[x+MESHX]+P[x+MESHX-1]-P[x+1]-P[x]);
+        du_dt = Mu*lap_u[z] - dp_dx - (u_old[z]*du_dx + v_old[z]*du_dy);
+        dv_dt = Mu*lap_v[z] - dp_dy - (u_old[z]*dv_dx + v_old[z]*dv_dy);
         u_now[z] = u_old[z] + deltat * u_old[z];
         v_now[z] = v_old[z] + deltat * v_old[z];
       }
     }
+    boundary1();
+    update(v_old, v_now);
+    update(u_old, u_now);
+    if((t%saveT) ==0) {
+      write2file(t,u_old,v_old, MESHX);
+    }    
   }
-  boundary();
-  update(v_old, v_now);
-  update(u_old, u_now);
+  printf("Yay! We are done.\n");
 }
 void update(double *old, double *new) {
   long i, j, z;
@@ -69,28 +75,17 @@ void update(double *old, double *new) {
     }
   }
 }
-
-void Pupdate(){
-  int i,j,z;
- 
-  for (i=1; i < MESHX-2; i++) {
-    for (j=1; j < MESHX-2; j++){
-      z= i*MESHX + j;
-      
-      old[z]=new[z];      
-    }
-  }
-}  
-void laplacian(double *f, double *lap) {
+  
+void laplacian(double *f, double *lap, int M) {
   long i,j,z;
     
-  for (i=1; i< MESHX -1; i++)
+  for (i=1; i< M -1; i++)
   {
-    for (j=1; j< MESHX -1; j++)
+    for (j=1; j< M -1; j++)
     {
       
-      z= i*MESHX + j;
-      lap[z] = (f[z-1] + f[z+1] + f[z+MESHX] + f[z-MESHX] -4.0*f[z])*inv_deltax2;
+      z= i*M + j;
+      lap[z] = (f[z-1] + f[z+1] + f[z+M] + f[z-M] -4.0*f[z])*inv_deltax2;
 
     }
   }
@@ -104,17 +99,27 @@ void initialize() {
   {
     for ( j=0; j < MESHX; j++)
     {
-// to be added
+      z= i*MESHX+j;
+      v_old[z] = 0.0;
+      u_old[z] = 0.0;
+    }
+  }
+  for ( i = 0; i < MESHX-1; i++)
+  {
+    for ( j=0; j < MESHX-1; j++)
+    {
+      z= i*MESHX+j;
+      P[z] = 0.0;
     }
   }
 }
-void boundary() {
+void boundary1() {
 
   int i ,y ,z; 
   for (i=0; i<MESHX; i++)
   {
     u_now[i] = Uw;
-    v_now[i] = 0.0
+    v_now[i] = 0.0;
     u_now[i*MESHX] = 0.0;
     v_now[i*MESHX] = 0.0;
     u_now[i*MESHX+MESHX-1] = 0.0;
@@ -127,47 +132,46 @@ void boundary() {
 void RHS_fn(double *u, double *v, double *lap_u, double *lap_v, int M){
   int i,j,x,z;
   double du_dx,du_dy,u_avg,d2u_dx2,d2u_dy2,d2u_dxdy,dv_dx,dv_dy,v_avg,d2v_dy2,d2v_dydx;
-  double advctn;
+  double advctn, dlapU_dx,dlapV_dy;
  
-  for (i=1; i<MESHX-2; i++){
-    for (j=1; j<MESHX-2; j++){
+  for (i=1; i<M-2; i++){
+    for (j=1; j<M-2; j++){
       
-      z = i*MESHX + j;
-      x = z - MESHX -1;
-      dlapU_dx =0.5*inv_deltax*(lap_u[z+1]-lap_u[z]+lap_u[z+MESHX+1]-lap_u[z+MESHX]);
-      dlapV_dy =0.5*inv_deltax*(lap_v[z+meshx]-lap_v[z]+lap_v[z+1+meshx]-lap_v[z+1]);
+      z = i*M + j;
+      dlapU_dx =0.5*inv_deltax*(lap_u[z+1]-lap_u[z]+lap_u[z+M+1]-lap_u[z+M]);
+      dlapV_dy =0.5*inv_deltax*(lap_v[z+M]-lap_v[z]+lap_v[z+1+M]-lap_v[z+1]);
 
-      du_dx = 0.5*inv_deltax*(u[z+i]-u[z]+u[z+MESHX+i]-u[z+MESHX]);
-      du_dy = 0.5*inv_deltax*(u[z+meshx]-u[z]+u[z+i+meshx]-u[z+i]);
-      u_avg = 0.25*(u[z]+u[z+i]+u[z+MESHX]+u[z+MESHX+i]);
-      d2u_dx2 = 0.25*inv_deltax2*(u[z-1]-u[z]-u[z+1]+u[z+2]+u[z-1+MESHX]-u[z+MESHX]-u[z+1+MESHX]+u[z+2+MESHX]);
-      d2u_dxdy = inv_deltax2*(u[z+MESHX+i]-u[z+i]-u[z+MESHX]+u[z]);
+      du_dx = 0.5*inv_deltax*(u[z+i]-u[z]+u[z+M+i]-u[z+M]);
+      du_dy = 0.5*inv_deltax*(u[z+M]-u[z]+u[z+i+M]-u[z+i]);
+      u_avg = 0.25*(u[z]+u[z+i]+u[z+M]+u[z+M+i]);
+      d2u_dx2 = 0.25*inv_deltax2*(u[z-1]-u[z]-u[z+1]+u[z+2]+u[z-1+M]-u[z+M]-u[z+1+M]+u[z+2+M]);
+      d2u_dxdy = inv_deltax2*(u[z+M+i]-u[z+i]-u[z+M]+u[z]);
 
-      dv_dx = 0.5*inv_deltax*(v[z+i]-v[z]+v[z+MESHX+i]-v[z+MESHX]);
-      dv_dy = inv_deltax*(v[z+meshx]-v[z]+v[z+i+meshx]-v[z+i]);
-      v_avg = 0.25*(v[z]+v[z+i]+v[z+MESHX]+v[z+MESHX+i]);
-      d2v_dy2 = 0.25*inv_deltax2*(v[z-MESHX]-v[z]-v[z+MESHX]+v[z+2*MESHX]+v[z-MESHX+i]-v[z+i]-v[z+MESHX+i]+v[z+2*MESHX+i]);
-      d2v_dydx = inv_deltax2*(v[z+MESHX+i]-v[z+MESHX]-v[z+i]+v[z]);
+      dv_dx = 0.5*inv_deltax*(v[z+i]-v[z]+v[z+M+i]-v[z+M]);
+      dv_dy = inv_deltax*(v[z+M]-v[z]+v[z+i+M]-v[z+i]);
+      v_avg = 0.25*(v[z]+v[z+i]+v[z+M]+v[z+M+i]);
+      d2v_dy2 = 0.25*inv_deltax2*(v[z-M]-v[z]-v[z+M]+v[z+2*M]+v[z-M+i]-v[z+i]-v[z+M+i]+v[z+2*M+i]);
+      d2v_dydx = inv_deltax2*(v[z+M+i]-v[z+M]-v[z+i]+v[z]);
 
       advctn = du_dx*du_dx+dv_dy*dv_dy+2*(dv_dx*du_dy)+v_avg*(d2v_dy2+d2u_dxdy)+u_avg*(d2u_dx2+d2v_dydx);
-      rhs_fn[x] = Mu*(dlapU_dx + dlapV_dy) - advctn;
+      rhs_fn[z] = Mu*(dlapU_dx + dlapV_dy) - advctn;
     }
   }
 
 }
-void write2file (long t, *phi) {
+void write2file (long t, double *u, double *v, int M) {
   int i,j,z;
   FILE *fp;
   char filename[1000];
-  sprintf(filename,"./datafiles2/phi_%ld.dat",t);
+  sprintf(filename,"./datafiles/velocity_%ld.dat",t);
   fp = fopen(filename,"w");
   if (fp) {
-    for ( i = 0; i < MESHX; i++)
+    for ( i = 0; i < M; i++)
     {
       for ( j=0; j < MESHX; j++)
       {
         z= i*MESHX + j;
-        fprintf(fp,"%d %d %le\n",i,j, phi[z]);
+        fprintf(fp,"%d %d %le %le\n",i,j, u[z],v[z]);
       }
       fprintf(fp,"\n");
     }
---
+++ fluid_solver/relaxation.c
@@ -0,0 +1,29 @@
+double relax(double *y,double *f,int mesh_tracker)
+{
+	double error=0.0;
+
+	
+
+	//printf("At level %d\n",mesh_tracker);
+	
+	
+	//============================================================================//
+	//solving the equation for the even update only(see "even_red_black_solver.c")
+		
+	even_rb_solve(y,f,&error,mesh_tracker);
+	//===========================================================================//	
+
+		
+	//============================================================================//
+	//solving the equation for the odd update only(see "odd_red_black_solver.c")
+		
+	odd_rb_solve(y,f,&error,mesh_tracker);
+	//===========================================================================//	
+	
+	//===========================================================================//
+	error=sqrt(error*d_sp[mesh_tracker].x*d_sp[mesh_tracker].y);
+	//===========================================================================//
+	
+	return error;
+	
+}
